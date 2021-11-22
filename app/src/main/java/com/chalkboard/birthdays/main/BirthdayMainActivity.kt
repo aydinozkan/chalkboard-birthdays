b6c5cca1bc444105fecfd60991798dd8dce7b707
Aydin Ozkan <aydinmehmetozkan@gmail.com>
2021-11-22 18:16:25 +0300

    Add main activity

---
+++ app/src/main/java/com/chalkboard/birthdays/main/BirthdayMainActivity.kt
@@ -0,0 +1,12 @@
+package com.chalkboard.birthdays.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chalkboard.birthdays.R
+
+class BirthdayMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_main)
+    }
+}
